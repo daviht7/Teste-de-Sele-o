2608993d4a6247601b2ef85288e9e7730b4bb66c
Davi Holanda <daviht7@gmail.com>
2020-02-16 20:03:59 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react';
+
+import  { 
+  Container,
+  Paragrafo,
+  BoxDia,
+  BoxDia2,
+  BoxMain,
+  Informar,
+  InformarDia,
+  ProdutoReceber,
+  AceitarTermos,
+  ButtonAssinante,
+  Main
+ } from './Style'
+
+import Global from './Global'
 
 function App() {
+
+  const [dias,setDias] = useState([])
+  const [dias2,setDias2] = useState([])
+  const [visibleDia,setVisibleDia] = useState(true)
+  const [diaSelecionado,setDiaSelecionado] = useState(0)
+  const [termoAceito,setTermoAceito] = useState(false)
+
+  useEffect(() => {
+
+    setDias([15,20,30,45,60]);
+    setDias2([90,105,120]);
+
+  },[])
+
+  useEffect(() => {
+
+    if(termoAceito) {
+      document.getElementById("btnAssinante").style.cursor = "pointer";
+    } else {
+      document.getElementById("btnAssinante").style.cursor = "not-allowed";
+    }
+
+  },[termoAceito])
+
+  useEffect(() => {
+
+    dias.forEach(dia => {
+      if(dia !== diaSelecionado) {
+        document.getElementById(dia).style.background = "#fff";
+      }
+    })
+    dias2.forEach(dia2 => {
+      if(dia2 !== diaSelecionado) {
+        document.getElementById(dia2).style.background = "#fff";
+      }
+    })
+
+  },[diaSelecionado,dias2,dias])
+
+  function selecionarDia(diaSelecionado) {
+    setDiaSelecionado(diaSelecionado)
+    document.getElementById(diaSelecionado).style.background = "blue";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main>
+    <Container>
+        <Paragrafo> Escolha o seu intervalo em dias </Paragrafo>
+
+      <BoxMain visible={visibleDia}>
+        { dias.map(dia => ( <BoxDia key={dia} id={dia} onClick={() => selecionarDia(dia)}> {dia} dias </BoxDia> )) }
+        { dias2.map(dia => ( <BoxDia2 key={dia} id={dia} onClick={() => selecionarDia(dia)}> {dia} dias </BoxDia2> )) }
+        </BoxMain>
+
+      <InformarDia visible={!visibleDia}>
+          <span>Digite o seu intervalo em dias</span>
+          <input 
+          type="number" 
+          placeholder="Ex: 12"
+          value={diaSelecionado}
+          onChange={e => setDiaSelecionado(e.target.value)}
+          />
+      </InformarDia>
+
+      <Informar onClick={() => setVisibleDia(!visibleDia)}> 
+        { visibleDia ? "Informar outro intervalo" : "Informar intervalo comuns" }
+      </Informar>
+
+      <AceitarTermos> 
+        <input type="checkbox" 
+        checked={termoAceito}
+        onChange={() => setTermoAceito(!termoAceito)}
+        />
+        Aceito os <u>termos e condições</u> 
+      </AceitarTermos>
+
+      <ProdutoReceber>
+          Você receberá seus produtos a cada: { 
+            diaSelecionado < 1 ? "0 dia" :
+            `${diaSelecionado < 1 ? "0" : diaSelecionado } 
+             ${diaSelecionado > 1 ? "dias" : "dia" } ` 
+          }
+      </ProdutoReceber>
+
+      <ButtonAssinante visible={termoAceito} id="btnAssinante">
+          Quero ser assinante
+      </ButtonAssinante>
+
+    </Container>
+    <Global />
+    </Main>
   );
 }
 
---
+++ src/Style.js
@@ -0,0 +1,126 @@
+import styled from "styled-components";
+import { darken } from "polished";
+
+export const Main = styled.div`
+  height: 100%;
+  min-height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Container = styled.div`
+  background: #fff;
+  display: flex;
+  flex-direction:column;
+  border:1px solid #000;
+  width:350px;
+  justify-content: center;
+  align-content:center;
+  padding:10px;
+`;
+
+export const Paragrafo  = styled.span`
+  color: #120a8f;
+  align-self: flex-start;
+  margin-bottom:10px;
+`;
+
+export const BoxDia = styled.button`
+  border: 1px solid #000;
+  margin:5px;
+  align-content:row;
+  border-radius:5px;
+  color:#000000;
+  padding:5px;
+  text-align:center;
+  width:40px;
+  font-size:10px;
+  background:#ffffff;
+
+`
+
+export const BoxDia2 = styled.button`
+  border: 1px solid #000;
+  margin:5px;
+  align-content:row;
+  border-radius:5px;
+  color:#000000;
+  padding:5px;
+  text-align:center;
+  width:70px;
+  font-size:10px;
+  background:#ffffff;
+`
+
+export const BoxMain = styled.div`
+    display:${props => (props.visible ? "flex" : "none")};
+    flex-direction:row;
+    flex-wrap: wrap;
+    align-items:center;
+    justify-content:center;
+`
+
+export const Informar = styled.button`
+  background-color:#fff;
+  color:#000;
+  border:0;
+  text-decoration:underline;
+  margin-top:15px;
+  width:50%;
+  align-self:center;
+`
+
+export const InformarDia = styled.div`
+
+  display:${props => (props.visible ? "flex" : "none")};
+  flex-direction:column;
+  margin:10px;
+  
+  input {
+
+    width:100%;
+    height:30px;
+    border-radius:4px;
+    background-color:#fff;
+
+  }
+
+`
+
+export const AceitarTermos = styled.div`
+
+  margin:10px;
+
+  u {
+    color:blue;
+  }
+
+`
+
+export const ProdutoReceber = styled.div`
+  border:none;
+  margin:10px;
+  border-top:1px solid #000;
+  border-bottom:1px solid #000;
+  padding:6px;
+  text-align:center;
+`
+
+export const ButtonAssinante = styled.button`
+
+  background:#00ff00;
+  height:30px;
+  font-size:15px;
+  border-radius:6px;
+  border:0;
+  disabled : false;
+  text-align:center;
+
+  &:hover {
+    background: ${darken(0.10, "#00ff00")};
+  }
+  
+
+
+`
